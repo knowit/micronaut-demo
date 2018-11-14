c3217d5170f018cc6d6162541bf96a396a882cbb
Fredrik Hov <fredrik.hov@knowit.no>
2018-11-15 00:10:49 +0100

    fix some sql and update submodules

--- database/initdb/init_tables.sql
+++ database/initdb/init_tables.sql
@@ -4,7 +4,6 @@ CREATE TABLE public.tasklist
     owner varchar(60) NOT NULL,
     title varchar(100) NOT NULL
 );
-CREATE UNIQUE INDEX tasklist_list_id_uindex ON public.tasklist (list_id);
 
 
 CREATE TABLE public.task
@@ -15,4 +14,3 @@ CREATE TABLE public.task
     status int NOT NULL,
     CONSTRAINT task_tasklist_list_id_fk FOREIGN KEY (parent_id) REFERENCES public.tasklist (list_id)
 );
-CREATE UNIQUE INDEX task_task_id_uindex ON public.task (task_id);
--- database/initdb/init_values.sql
+++ database/initdb/init_values.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.tasklist (list_id, owner, title) VALUES (1, 'jvm-chapter', 'Demo-list');
-INSERT INTO public.tasklist (list_id, owner, title) VALUES (2, 'jvm-chapter', 'Chapter Todo');
-INSERT INTO public.tasklist (list_id, owner, title) VALUES (3, 'security-chapter', 'Chapter Todo');
+INSERT INTO public.tasklist (owner, title) VALUES ('jvm-chapter', 'Demo-list');
+INSERT INTO public.tasklist (owner, title) VALUES ('jvm-chapter', 'Chapter Todo');
+INSERT INTO public.tasklist (owner, title) VALUES ('security-chapter', 'Chapter Todo');
